9326e062ed6deed179b560ccf520b85e6df41da5
ManaiahJ <manaiah.j19988@gmail.com>
2024-08-25 14:47:13 +0530

    This is the first change in Project.

---
+++ MSStockAlert/src/test/java/com/ms/Manu/MsStockAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.Manu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStockAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
